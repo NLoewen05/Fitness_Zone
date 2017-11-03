1436d259ea1e06e61d220588caec2680fb0de683
Nicolas Loewen <nic_loewen@yahoo.ca>
2017-11-03 15:47:53 +0000

    Created the controller and view entries to allow users to sign up with more than just their email. Includes name and username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:firstName, :lastName, :username, :password, :password_confirmation, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstName, :lastName, :username, :email, :password, :password_confirmation])
+
   end
 
 end
